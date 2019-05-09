78a4e3de05f15e6b7210c1af074145f97a9ea8b7
aragaer <aragaer@gmail.com>
2019-05-09 22:29:18 +0300

    Forgotten files

---
+++ bindiff.h
@@ -0,0 +1,12 @@
+#ifndef _BINDIFF_H_
+#define _BINDIFF_H_
+
+typedef struct diff_s {
+  char *first, *second;
+  size_t size;
+  struct diff_s *next;
+} diff_t;
+
+diff_t *compare(char *first, char *second, size_t size);
+
+#endif  // _BINDIFF_H_
---
+++ compare.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+#include <bindiff.h>
+
+diff_t *compare(char *first, char *second, size_t size) {
+  if (size == 0)
+    return NULL;
+  diff_t *result = malloc(sizeof(diff_t));
+  result->size = 1;
+  result->next = NULL;
+  result->first = first;
+  result->second = second;
+  return result;
+}
